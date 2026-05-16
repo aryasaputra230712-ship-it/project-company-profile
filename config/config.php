63db858b945c0657c5d10657bbe0541b4b99833d
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-17 07:40:09 +0800

    membuat config

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost"; // Tetap localhost karena kodingan & DB sama-sama di hosting
+$user = "vibewebs_id_rsa"; // User yang kamu buat di cPanel
+$pass = "Vibewebs0708"; // Password yang kamu reset tadi
+$db   = "vibewebs_db"; // Nama database di cPanel
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+?>
