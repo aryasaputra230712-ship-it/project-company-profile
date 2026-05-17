aa4d226daccd27e8f21937b543ab98039f86f777
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-17 10:52:18 +0800

    Auto commit

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ while ($row = mysqli_fetch_assoc($query)) {
         </div>
     </div>
 </section>
-hello world bro aosdnald dopajpfiasd oijdadaos dasfasdas
+hello world
 <script>
     window.dataSlides = <?php echo json_encode($slides); ?>;
 </script>
