e24cc35fb77c006f83011118c00d92dfd7b6464e
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-17 10:15:30 +0800

    test

--- config/config.php
+++ config/config.php
@@ -5,8 +5,8 @@ $user = "vibewebs_id_rsa"; // User yang kamu buat di cPanel
 $pass = "Vibewebs0708"; // Password yang kamu reset tadi
 $db   = "vibewebs_db"; // Nama database di cPanel
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db);
 
-if (!$koneksi) {
+if (!$conn) {
     die("Koneksi gagal : " . mysqli_connect_error());
 }
