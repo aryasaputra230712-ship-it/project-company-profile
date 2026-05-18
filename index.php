1e4d1428ec8c612320ad26fe9433fd6919c981ab
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-18 15:14:38 +0800

    mengubah bagian index

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-define('BASE_URL', '');
-define('ROOTPATH', __DIR__);
+if (!defined('ROOTPATH')) {
+    define('ROOTPATH', __DIR__);
+}
+
+// 2. Buat Base URL Otomatis (Bisa mendeteksi folder /company_profile/ secara mandiri)
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https" : "http";
+$host = $_SERVER['HTTP_HOST'];
+$script_name = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+$base_url = $protocol . "://" . $host . ($script_name == '/' ? '' : $script_name);
+
+// Definisikan konstanta agar bisa dipakai di seluruh file
+define('BASE_URL', $base_url);
 
 include_once ROOTPATH . "/config/config.php";
-$page_css = "/main";
+$page_css = "main";
 
 // 1. LOGIK DATABASE
 $query = mysqli_query($conn, "SELECT * FROM slide_utama WHERE status = 'active'");
