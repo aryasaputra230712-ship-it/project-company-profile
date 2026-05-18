cb258ff268937a424fb313fafc927c38ab820ba9
YogzzChiew <yogaarya825@gmail.com>
2026-05-18 16:07:13 +0800

    mengedit footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,8 +1,21 @@
+<?php
+// 2. Deteksi Protokol(Jangan di otak atik)
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ||
+    (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+    ? "https" : "http";
+
+// 3. Base URL Pintar
+// Ini akan menghasilkan "vibewebs.web.id" di hosting
+// Dan "localhost/company_profile" di laptop kamu
+$host = $_SERVER['HTTP_HOST'];
+$script_name = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+$base_url = $protocol . "://" . $host . ($script_name == '/' ? '' : $script_name);
+?>
 
     <footer style="background: rgb(17 43 59); color: white; padding: 50px 0;">
         <div style="display: flex; justify-content: space-around;">
             <div>
-                <img src="../../assets/imgs/logo_gold.png" alt="Logo" width="100">
+                <img src="<?= $base_url ?> /assets/imgs/logo_gold.png" alt="Logo" width="100">
                 <p>blalbablablablaballbalbal</p>
             </div>
 
