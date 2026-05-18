3d199cf3e780720ed3bc2bb8aa2ba65d66ed30e2
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-18 10:54:38 +0800

    perubahan code untuk memudahkan

--- index.php
+++ index.php
@@ -1,27 +1,24 @@
 <?php
-// Perbaikan path untuk hosting
+
 define('ROOTPATH', __DIR__);
-define('BASE_URL', '');
 
-$page_css = "index";
-
-// Memastikan file config terpanggil dengan benar
 include_once ROOTPATH . "/config/config.php";
 
+$page_css = "main"; // jika styling khusus main.php di main.css
 
 $query = mysqli_query($conn, "SELECT * FROM slide_utama WHERE status = 'active'");
 $slides = [];
 while ($row = mysqli_fetch_assoc($query)) {
     $slides[] = $row;
 }
+
 include_once ROOTPATH . "/layouts/header.php";
-require ROOTPATH . "/modules/main.php";
-
-
+include_once ROOTPATH . "/modules/main.php";
 ?>
+
 <script>
     window.dataSlides = <?php echo json_encode($slides); ?>;
 </script>
-<script src="../../assets/js/main.js"></script>
+<script src="<?php echo isset($base_url) ? $base_url : ''; ?>/assets/js/main.js"></script>
 
 <?php include_once ROOTPATH . "/layouts/footer.php"; ?>
