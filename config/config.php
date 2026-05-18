403dd97f207a5bcb4e2868e33546607cd80ca804
YogzzChiew <yogaarya825@gmail.com>
2026-05-18 15:01:30 +0800

    test config

--- config/config.php
+++ config/config.php
@@ -1,9 +1,21 @@
 <?php
 
-$host = "185.151.49.65"; // Tetap localhost karena kodingan & DB sama-sama di hosting
-$user = "vibewebs_id_rsa"; // User yang kamu buat di cPanel
-$pass = "Aryasaputra23"; // Password yang kamu reset tadi
-$db   = "vibewebs_db"; // Nama database di cPanel
+// Mengecek apakah server dijalankan secara lokal (localhost)
+if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1' || $_SERVER['REMOTE_ADDR'] == '::1') {
+
+    // Jika dijalankan di komputer lokal, gunakan konfigurasi lokal
+    $host = "localhost";        // Alamat server database lokal
+    $user = "root";             // Username MySQL lokal
+    $password = ""; // Password MySQL lokal
+    $database = "poin_pelanggaran"; // Nama database lokal
+
+} else {
+    // Jika dijalankan di jaringan (bukan localhost), gunakan konfigurasi server
+    $host = "185.151.49.65"; // Tetap localhost karena kodingan & DB sama-sama di hosting
+    $user = "vibewebs_id_rsa"; // User yang kamu buat di cPanel
+    $pass = "Aryasaputra23"; // Password yang kamu reset tadi
+    $db   = "vibewebs_db"; // Nama database di cPanel
+}
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
