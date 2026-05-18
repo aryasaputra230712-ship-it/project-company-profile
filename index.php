8c5ac2abd6a746826f8f1df5a4abf07b83392662
Arya Dwi <aryasaputra2334@gmail.com>
2026-05-18 14:26:41 +0800

    test

--- index.php
+++ index.php
@@ -13,9 +13,27 @@ while ($row = mysqli_fetch_assoc($query)) {
 }
 
 include ROOTPATH . "/layouts/header.php";
-include ROOTPATH . "/modules/main.php";
+
 
 ?>
+
+<main>
+    <div class="container-main">
+        <div class="main-intro">
+            <div class="video-bg-container">
+                <video class="video-bg" muted autoplay loop playsinline>
+                    <source src="<?= BASE_URL ?>/assets/videos/<?= $slides[0]['video_file']; ?>" type="video/mp4">
+                </video>
+            </div>
+
+            <div class="intro-content">
+                <img src="<?= BASE_URL ?>/assets/imgs/logo.png" alt="Logo">
+                <h1 id="dynamic-title"><?= $slides[0]['judul']; ?></h1>
+                <p id="dynamic-subtitle"><?= $slides[0]['subjudul']; ?></p>
+            </div>
+        </div>
+    </div>
+</main>
 
 <script>
     window.dataSlides = <?php echo json_encode($slides); ?>;
