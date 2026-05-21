88f195219457c19d78a636653ace45d19f1b0b1e
YogzzChiew <yogaarya825@gmail.com>
2026-05-22 00:22:46 +0800

    merubah warna bg footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,21 +1,21 @@
 
-        <footer class="bg-white text-black py-[60px] px-6">
+        <footer class="bg-aurelis-blue py-[60px] px-6">
             <div class="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 max-w-[77rem] gap-12 m-auto uppercase">
                 <div>
                     <img class="w-[100px] mb-6" src="<?= BASE_URL ?>/assets/imgs/logo_gold.png" alt="brand-footer">
-                    <p class="text-sm">Aurelis Jewelry is dedicated to crafting timeless jewelry pieces that embody elegance, luxury, and exceptional craftsmanship.</p>
+                    <p class="text-sm font-thin">Aurelis Jewelry is dedicated to crafting timeless jewelry pieces that embody elegance, luxury, and exceptional craftsmanship.</p>
                 </div>
 
                 <div class="text-sm">
                     <p class="text-2xl mb-4">CONTACT</p>
-                    <p class="mb-1">Phone: +62 812 3456 7890</p>
-                    <p class="mb-1">Email: info@aurelis.com</p>
-                    <p>Denpasar, Bali, Indonesia</p>
+                    <p class="mb-1 font-thin">Phone: +62 812 3456 7890</p>
+                    <p class="mb-1 font-thin">Email: info@aurelis.com</p>
+                    <p class="font-thin">Denpasar, Bali, Indonesia</p>
                 </div>
 
                 <div>
                     <p class="text-2xl mb-5">Follow Us</p>
-                    <div class="flex gap-7 text-xl">
+                    <div class="flex gap-7 text-xl ">
                         <a href="#"><i class="fa-brands fa-instagram rounded-[50%] border border-black p-2 transition-all duration-300 hover:bg-blue-400"></i></a>
                         <a href="#"><i class="fa-brands fa-facebook rounded-[50%] border border-black p-2 transition-all duration-300 hover:bg-blue-400"></i></a>
                         <a href="#"><i class="fa-brands fa-tiktok rounded-[50%] border border-black p-2 transition-all duration-300 hover:bg-blue-400"></i></a>
